356fb76257b5d43123a225fb9ce8bf0a193f298e
eraltafs <43698833+eraltafs@users.noreply.github.com>
2023-04-04 14:52:33 +0530

    resume button added

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ GitHubCalendar(".react-activity-calendar", "eraltafs", { responsive: true });
 
 document.getElementById("resume-button-2").onclick = ()=>{
     // location.href= "\\static\\pdf\\Altaf_Khan_Resume.pdf"
-    window.open("\\static\\pdf\\Altaf_Khan_Resume.pdf")
+    window.open("\\static\\pdf\\Altaf_Khan_Resume.pdf",'_blank')
      
 }
 document.getElementById("resume-button-1").onclick = ()=>{
     // location.href= "\\static\\pdf\\Altaf_Khan_Resume.pdf"
-    window.open("\\static\\pdf\\Altaf_Khan_Resume.pdf")
+    window.open("\\static\\pdf\\Altaf_Khan_Resume.pdf",'_blank')
 }
