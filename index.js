e07e451744dbc38544bccd22507d23580da7f194
eraltafs <teacheraltafs@gmail.com>
2024-04-30 13:18:28 +0530

    react added

--- index.js
+++ index.js
@@ -4,29 +4,27 @@ let navMenu = document.querySelector("#nav-menu");
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 }
-
 
 let navLink = document.querySelectorAll(".nav-link");
 
-navLink.forEach(n => n.addEventListener("click", closeMenu));
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
 }
-
 
 // GitHubCalendar(".react-activity-calendar", "eraltafs")
 GitHubCalendar(".react-activity-calendar", "eraltafs", { responsive: true });
 
-document.getElementById("resume-button-1").onclick = ()=>{
-    // location.href= "\\static\\pdf\\Altaf_Khan_Resume.pdf"
-    window.open("\\static\\pdf\\Altaf_Khan_Resume.pdf",'_blank')
-}
-document.getElementById("resume-button-2").onclick = ()=>{
-    // location.href= "\\static\\pdf\\Altaf_Khan_Resume.pdf"
-    window.open("\\static\\pdf\\Altaf_Khan_Resume.pdf",'_blank')
-}
+document.getElementById("resume-button-1").onclick = () => {
+  // location.href= "\\static\\pdf\\Altaf_Khan_Resume.pdf"
+  window.open("\\static\\pdf\\Altaf_Khan_Resume.pdf", "_blank");
+};
+document.getElementById("resume-button-2").onclick = () => {
+  // location.href= "\\static\\pdf\\Altaf_Khan_Resume.pdf"
+  window.open("\\static\\pdf\\Altaf_Khan_Resume.pdf", "_blank");
+};
